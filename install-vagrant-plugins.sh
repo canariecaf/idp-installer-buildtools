9673642febe65270dc78cd877c63465398662981
Chris Phillips <chris.phillips@canarie.ca>
2016-05-03 20:41:26 -0400

    tweaks to vagrant plugin installation windows

--- install-vagrant-plugins.sh
+++ install-vagrant-plugins.sh
@@ -4,11 +4,19 @@ set -e
 set -u 
 
 PLAT=`uname`
-
 echo -e "\nOne moment checking for plugins on your OS (${PLAT})"
 
-echo -e "Forcing plugin installation for vagrant-hostmanager and vagrant-vbguest"
 
+PREVRUNTEST=0
+
+function detectPreviousRun ()
+{
+  echo -e "Next we need to read the /etc/sudoers file as root to check if we have applied our updates"
+  echo -e "If we have, we will skip updates, if not we will apply sudo settings to update /etc/hosts"
+  PREVRUNTEST=`sudo cat /etc/sudoers |grep VAGRANT_HOSTMANAGER_UPDATE|wc -l`
+}
+
+echo -e "Forcing plugin installation for vagrant-hostmanager and vagrant-vbguest"
 vagrant plugin install vagrant-hostmanager
 vagrant plugin install vagrant-vbguest
 
@@ -16,50 +24,47 @@ echo -e ""
 echo -e "Installed Plugins:"
 vagrant plugin list
 echo -e ""
-
-
-echo -e "Next we need to read the /etc/sudoers file as root to check if we have applied our updates"
-echo -e "If we have, we will skip updates, if not we will apply sudo settings to update /etc/hosts"
-
-PREVRUNTEST=`sudo cat /etc/sudoers |grep VAGRANT_HOSTMANAGER_UPDATE|wc -l`
-
-
-if [ ${PREVRUNTEST} -gt 0 ]; then
-
-  echo "We detected a previous update, skipping editting the /etc/sudoers file"
-
-else
-
-echo "Beginning to edit the /etc/sudoers file"
   
+
   case `uname` in
     Darwin)
-      echo -e "Updating the /etc/sudoers file"
+
+  detectPreviousRun
+  if [ ${PREVRUNTEST} -gt 0 ]; then
+    echo "We detected a previous update, skipping editting the /etc/sudoers file"
+  else
+     echo -e "Updating the /etc/sudoers file"
       sudo tee -a /etc/sudoers <<EOF
 Cmnd_Alias VAGRANT_HOSTMANAGER_UPDATE = /bin/cp $HOME/.vagrant.d/tmp/hosts.local /etc/hosts
 %admin ALL=(root) NOPASSWD: VAGRANT_HOSTMANAGER_UPDATE
 EOF
+
+fi
       ;;
     Linux)
-      if [ -f /etc/debian_version ]; then
-        sudo apt-get install -y unzip curl
-      fi
+
+  detectPreviousRun
+  if [ ${PREVRUNTEST} -gt 0 ]; then
+    echo "We detected a previous update, skipping editting the /etc/sudoers file"
+  else
+     echo -e "Updating the /etc/sudoers file"
+
       sudo tee -a /etc/sudoers <<EOF
 Cmnd_Alias VAGRANT_HOSTMANAGER_UPDATE = /bin/cp $HOME/.vagrant.d/tmp/hosts.local /etc/hosts
 %admin ALL=(root) NOPASSWD: VAGRANT_HOSTMANAGER_UPDATE
 EOF
+fi  
       ;;
     CYGWIN*)
 
-echo "on Windows using cygwin - No edits performed. Ensure you have git, curl, and unzip installed via setup"
+echo "Windows using cygwin - No edits performed. Ensure you have git, curl, and unzip installed via setup"
       ;;
       MING*)
 
-echo "on Windows using GIT bash - No edits performed. Ensure you have git, curl, and unzip installed"
+echo "Windows using GIT bash - No edits performed."
       ;;
   esac
  
-
 fi
 echo -e "install-vagrant-plugins.sh run complete"
   
