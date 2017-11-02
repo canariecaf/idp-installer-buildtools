821af3c6dc6ba1467a0c7dae211cb20b86938ed6
Chris Phillips <chris.phillips@canarie.ca>
2017-11-01 23:07:16 -0400

    updated base IdP-Installer version

--- idp/bootstrap.sh
+++ idp/bootstrap.sh
@@ -9,7 +9,7 @@ mkdir -p work
 cd work
 GITOWNER="canariecaf"
 GITREPO="idp-installer-CAF"
-GITBRANCH="3.0.0-CAF"
+GITBRANCH="3.1.0-CAF-RC1"
 GITROOT="https://github.com/${GITOWNER}/${GITREPO}"
 
 CFGTEMPLATE="${r}/config.template"
